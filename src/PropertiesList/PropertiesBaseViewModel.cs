c79978b7e6c62cbb24e22eeaa3fca735294efffc
jbautistam <jbautistam@gmail.com>
2025-03-16 14:43:57 +0100

    New solution

---
+++ src/PropertiesList/PropertiesBaseViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bau.Controls.PropertiesList;
+
+public abstract class UciOption
+{
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string DefaultValue { get; set; }
+}
+
+public class CheckOption : UciOption
+{
+public bool Value { get; set; }
+
+public CheckOption()
+{
+    Type = "check";
+}
+}
+
+public class SpinOption : UciOption
+{
+public int MinValue { get; set; }
+public int MaxValue { get; set; }
+public int Value { get; set; }
+
+public SpinOption()
+{
+    Type = "spin";
+}
+}
+
+public class ComboOption : UciOption
+{
+public List<string> ComboValues { get; set; }
+public string SelectedValue { get; set; }
+
+public ComboOption()
+{
+    Type = "combo";
+}
+}
+
+
---
+++ src/PropertiesList/PropertiesListViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Bau.Controls.PropertiesList;
+
+/// <summary>
+///     ViewModel para una lista de propiedades
+/// </summary>
+public class PropertiesListViewModel : INotifyPropertyChanged
+{
+    // Eventos públicos
+    public event PropertyChangedEventHandler? PropertyChanged;
+    // Variables privadas
+    private ObservableCollection<UciOption> _options;
+
+    public ObservableCollection<UciOption> Options
+    {
+        get => _options;
+        set
+        {
+            _options = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public PropertiesListViewModel()
+    {
+        // Inicializa con opciones de ejemplo
+        Options = new ObservableCollection<UciOption>
+        {
+            new SpinOption { Name = "Hash", MinValue = 1, MaxValue = 128, Value = 16 },
+            new CheckOption { Name = "Ponder", Value = false },
+            new ComboOption { Name = "Style", ComboValues = new List<string> { "Solid", "Aggressive", "Passive" }, SelectedValue = "Solid" }
+        };
+    }
+
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
